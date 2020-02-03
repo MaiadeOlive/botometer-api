413edf281103300c7d027b222312e4d2c4dc6a5c
Fernanda Maia de Oliveira <fernanda.oliveira@softplan.com.br>
2020-02-03 13:28:45 -0300

    coment port, debud and host

--- app.py
+++ app.py
@@ -78,6 +78,6 @@ def download_zip():
     return send_file(path, as_attachment=True) 
     
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000, host='0.0.0.0')
 
